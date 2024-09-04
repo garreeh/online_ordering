a26eb6b6119f14a451ae8f4526d5a7fe18439ccb
Garry Gajultos <gajultos.garry123@gmail.com>
2024-09-04 18:22:15 +0800

    added SQL

--- ecommerce.sql
+++ ecommerce.sql
@@ -11,7 +11,7 @@
  Target Server Version : 100432
  File Encoding         : 65001
 
- Date: 03/09/2024 18:31:10
+ Date: 04/09/2024 18:21:42
 */
 
 SET NAMES utf8mb4;
@@ -53,13 +53,14 @@ CREATE TABLE `category`  (
   `created_at` timestamp(0) NOT NULL DEFAULT current_timestamp(0),
   `updated_at` timestamp(0) NOT NULL DEFAULT current_timestamp(0) ON UPDATE CURRENT_TIMESTAMP(0),
   PRIMARY KEY (`category_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of category
 -- ----------------------------
-INSERT INTO `category` VALUES (1, 'testttttqweqwe', '2024-05-14 17:48:46', '2024-05-14 17:49:08');
-INSERT INTO `category` VALUES (2, 'Dog Food', '2024-05-14 17:48:30', '2024-05-16 10:58:50');
+INSERT INTO `category` VALUES (1, 'Dog Food Ni Bert', '2024-05-14 17:48:46', '2024-09-04 17:25:31');
+INSERT INTO `category` VALUES (2, 'Dog Food Ni Patrick', '2024-05-14 17:48:30', '2024-09-04 17:25:39');
+INSERT INTO `category` VALUES (5, 'Test lang', '2024-09-04 17:23:29', '2024-09-04 17:25:44');
 
 -- ----------------------------
 -- Table structure for payment
@@ -106,18 +107,20 @@ CREATE TABLE `product`  (
   `category_id` int(255) NULL DEFAULT NULL,
   `product_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `product_description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `barcode` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `product_image` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `product_sku` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `product_stocks` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `product_unitprice` decimal(10, 2) NULL DEFAULT NULL,
   `product_sellingprice` decimal(10, 2) NULL DEFAULT NULL,
   `created_at` timestamp(0) NOT NULL DEFAULT current_timestamp(0),
   `updated_at` timestamp(0) NOT NULL DEFAULT current_timestamp(0) ON UPDATE CURRENT_TIMESTAMP(0),
   PRIMARY KEY (`product_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 12 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of product
 -- ----------------------------
+INSERT INTO `product` VALUES (11, NULL, NULL, 'first data', 'first data', '../../uploads/4.png', 'first data', NULL, 0.00, 0.00, '2024-09-04 18:14:57', '2024-09-04 18:14:57');
 
 -- ----------------------------
 -- Table structure for purchase_order
@@ -155,25 +158,16 @@ CREATE TABLE `supplier`  (
   `mobile_number` int(11) NULL DEFAULT NULL,
   `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `tin` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `contact_person` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `type_of_company` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `terms` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `payment` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `mode_of_delivery` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `vatable` enum('Yes','No') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `created_at` timestamp(0) NOT NULL DEFAULT current_timestamp(0),
   `updated_at` timestamp(0) NOT NULL DEFAULT current_timestamp(0) ON UPDATE CURRENT_TIMESTAMP(0),
   PRIMARY KEY (`supplier_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 25 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 28 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of supplier
 -- ----------------------------
-INSERT INTO `supplier` VALUES (18, 'garssseh', '123456', 123123, 0, 'gajultos.garry123@gmail.com', '123', '123', '123', '123', '123', 'test', 'Yes', '2024-04-13 15:55:11', '2024-05-13 16:08:14');
-INSERT INTO `supplier` VALUES (20, 'garreeh', 'test', 0, 123, 'gajultos.garry123@gmail.com', '1', '1', '1', '1', '1', 'test', 'Yes', '2024-05-08 16:32:56', '2024-05-08 17:51:28');
-INSERT INTO `supplier` VALUES (22, 'test', '123', 123, 123, 'gajultos.garrydev@gmail.com', '111', '123', '123', '123', '123', '123', 'Yes', '2024-05-16 10:57:32', '2024-07-30 10:28:35');
-INSERT INTO `supplier` VALUES (23, '123', '123', 3123, 3213, 'gajultos.garry123@gmail.com', '1231', '123123', '123132', '123123', '123', '123', 'Yes', '2024-05-20 15:38:40', '2024-05-20 15:38:40');
-INSERT INTO `supplier` VALUES (24, 'qwe', 'qwe', 0, 0, 'gajultos.garrydev@gmail.com', 'qew', 'qwe', '123', '213', '123444', '123', 'Yes', '2024-07-30 10:28:25', '2024-07-30 10:28:25');
+INSERT INTO `supplier` VALUES (26, 'test', '1', 1, 123, 'gajultos.garrydev@gmail.com', '123', '2024-09-04 14:22:47', '2024-09-04 15:01:43');
+INSERT INTO `supplier` VALUES (27, '2', '2', 2, 2323, 'tanginathis213012@gmail.com', '23', '2024-09-04 14:22:53', '2024-09-04 15:01:53');
 
 -- ----------------------------
 -- Table structure for users
@@ -190,22 +184,19 @@ CREATE TABLE `users`  (
   `remember_me` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `created_at` timestamp(0) NOT NULL DEFAULT current_timestamp(0),
   `updated_at` timestamp(0) NOT NULL DEFAULT current_timestamp(0) ON UPDATE CURRENT_TIMESTAMP(0),
-  `user_type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `user_type_id` int(11) NULL DEFAULT NULL,
   `is_admin` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `account_status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `user_address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`user_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 15 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 33 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of users
 -- ----------------------------
-INSERT INTO `users` VALUES (1, 'Garry Gajultos', 'garry', 'gajultos.garrydev@gmail.com', 123123123, '$2y$10$qLpIqQapwBy.Z5akQkYgkuT/8JFBv460vuqIFKUGyvp2l58YTeRvW', '123123', '', '2024-04-07 16:08:00', '2024-09-03 18:28:27', NULL, '1', NULL, '1');
-INSERT INTO `users` VALUES (2, 'Test Account', 'Ron', 'gajultos.garry123@gmail.com', NULL, '$2y$10$Wtj4pYEWKXHYe4DUwLPTveZdPJUNrXwfkfeZRWXO4bnmbNd9NOA9y', 'test1005', NULL, '2024-05-13 18:18:17', '2024-09-03 10:53:31', NULL, '0', NULL, NULL);
-INSERT INTO `users` VALUES (8, '1', 'not admin', 'pajamaribel17@gmail.com', 1, '$2y$10$IS1QJYBN1EP0ATfdxDN49O8/P9EA/2rBD4GyXyt3A7o7OOhx94zN6', '1', NULL, '2024-09-03 13:16:08', '2024-09-03 13:16:08', NULL, '0', NULL, NULL);
-INSERT INTO `users` VALUES (10, 'Garry Gajultos', '1', '123123@gmail.c', 123, '$2y$10$mo8rpOmdnJJCvP/hpQU3kubL9iVLW8X/cZVEIXvA4J4t/TXKsGPFG', '123', NULL, '2024-09-03 15:31:48', '2024-09-03 15:31:48', NULL, '0', NULL, NULL);
-INSERT INTO `users` VALUES (13, '123', '123', '11amethyst.cerbitojelojean@gmail.com', 1, '$2y$10$d.5kLM.9RzSWiVzZSE7b5uxp3nCx1hlqn0Al55xkcvFeKjOZW3jte', '1', NULL, '2024-09-03 17:00:42', '2024-09-03 17:00:42', NULL, '1', 'Inactive', '123');
-INSERT INTO `users` VALUES (14, 'qwe', 'qwe', '123123@gmail.com', 1, '$2y$10$LblabnZoC0VN9mUeApeJ5uF957Ok1Q841SGDl4VIPT7T97ElXyMgi', '1', NULL, '2024-09-03 18:17:28', '2024-09-03 18:17:28', NULL, '1', 'Inactive', '1');
+INSERT INTO `users` VALUES (1, 'Garry Gajultos', 'garry', '123123@gmail.com', 123123123, '$2y$10$vtj3lvgROA.ecVm2oI2YnOrlhFzn1jNE/sMk72HTTH.ymfaBol9jW', '123123', '', '2024-04-07 16:08:00', '2024-09-04 15:57:51', NULL, '1', 'Active', '1');
+INSERT INTO `users` VALUES (2, 'Test Account', 'Ron', '123123@gmail.comm', NULL, '$2y$10$Wtj4pYEWKXHYe4DUwLPTveZdPJUNrXwfkfeZRWXO4bnmbNd9NOA9y', 'test1005', NULL, '2024-05-13 18:18:17', '2024-09-04 15:58:23', NULL, '0', 'Active', NULL);
+INSERT INTO `users` VALUES (32, '123', '123', 'pendragondeveloper@gmail.com', 1, '$2y$10$lCWQG/EL9ErCmkNN12/2HulQ3x.XDscSUhdfNN6xWGGOkTd.6FKLa', '1', NULL, '2024-09-04 16:32:35', '2024-09-04 16:51:53', NULL, '0', 'Inactive', NULL);
 
 -- ----------------------------
 -- Table structure for usertype
@@ -214,28 +205,19 @@ DROP TABLE IF EXISTS `usertype`;
 CREATE TABLE `usertype`  (
   `user_type_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_type_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `created_at` timestamp(0) NULL DEFAULT NULL,
-  `updated_at` timestamp(0) NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP(0),
+  `created_at` timestamp(0) NULL DEFAULT current_timestamp(0),
+  `updated_at` timestamp(0) NULL DEFAULT current_timestamp(0) ON UPDATE CURRENT_TIMESTAMP(0),
+  `inventory_module` enum('1','0') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '1',
+  `user_module` enum('1','0') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '1',
+  `reports_module` enum('1','0') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '1',
+  `po_module` enum('1','0') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '1',
   PRIMARY KEY (`user_type_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of usertype
 -- ----------------------------
-
--- ----------------------------
--- Table structure for withdraw
--- ----------------------------
-DROP TABLE IF EXISTS `withdraw`;
-CREATE TABLE `withdraw`  (
-  `withdraw_id` int(11) NOT NULL AUTO_INCREMENT,
-  `billing_id` int(11) NULL DEFAULT NULL,
-  `product_id` int(11) NULL DEFAULT NULL,
-  PRIMARY KEY (`withdraw_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
-
--- ----------------------------
--- Records of withdraw
--- ----------------------------
+INSERT INTO `usertype` VALUES (2, 'Admin', '2024-09-04 10:46:35', '2024-09-04 17:09:22', '0', '1', '1', '1');
+INSERT INTO `usertype` VALUES (3, 'Encoder', '2024-09-04 10:46:46', '2024-09-04 10:46:46', '1', '1', '1', '1');
 
 SET FOREIGN_KEY_CHECKS = 1;
