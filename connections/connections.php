8f27630b99c85ac3fc64ba125e11421c52c0669d
Garry Gajultos <gajultos.garry123@gmail.com>
2024-11-24 23:05:32 +0800

    LAST COMMIT FOR DB

--- connections/connections.php
+++ connections/connections.php
@@ -1,14 +1,14 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "ecommerce";
-
 // $servername = "localhost";
-// $username = "u759574209_online_order";
-// $password = "Mybossrocks081677!";
-// $dbname = "u759574209_online_order";
+// $username = "root";
+// $password = "";
+// $dbname = "ecommerce";
+
+$servername = "localhost";
+$username = "u759574209_online_order";
+$password = "Mybossrocks081677!";
+$dbname = "u759574209_online_order";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
